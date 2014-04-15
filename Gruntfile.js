4f778d4da2477af1c48f1676dab8bd862c2089e0
arturfelipe <arturfelipe.sousa@gmail.com>
2014-04-15 01:11:42 -0300

    add some basic grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,28 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
-
-        pkg: grunt.file.readJSON('package.json')
-
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            server: {
+                options: {
+                    port: 9001,
+                    base: '.',
+                    keepalive: true
+                }
+            }
+        },
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'assets/css/main.css':'app/css/main.less'
+                }
+            }
+        }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-less');
 };
